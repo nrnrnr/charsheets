3d7d1201c89007dd5ad52cc55c2b11da6b0931d3
Norman Ramsey <nr@cs.tufts.edu>
2025-08-30 21:17:12 -0400

    dreamhost workarounds and other changes
    dreamhost is not actually viable but there's no reason to revert these changes

---
+++ character-sheets/halligan-prefix.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export CHARSHEET_CMD=/h/nr/www/charsheet/charsheet
+export CHARSHEETS=/h/nr/www/charsheet/
+export TEXINPUTS=.:/h/nr/www/charsheet/:
+export PATH="/home/nr/bin:/usr/sup/bin:$PATH"
+export LUA_PATH="/h/nr/src/lua/?.lua;/h/nr/src/lua/?/init.lua;/h/nr/lib/lua/5.1/?.lua;/usr/unsup/nr/lib/lua5.1/?.lua;/usr/unsup/nr/lib/lua5.1/?/init.lua;/h/nr/.luarocks/share/lua/5.1/?/init.lua;/h/nr/.luarocks/share/lua/5.1/?.lua;;"
+export LUA_CPATH="/h/nr/machine/amd64-linux/lib/?.so;/h/nr/machine/amd64-linux/lib/lua5.1/?.so;/usr/unsup/nr/lib/lua5.1/?.so;/h/nr/.luarocks/lib/lua/5.1/?.so;;"
+
+DEBUG="PATH=$PATH
+LUA_PATH=$LUA_PATH
+LUA_CPATH=$LUA_CPATH
+which lua.51: $(type lua5.1)"
+
+if [[ -x /usr/sup/bin/lua5.1 ]]; then
+  DEBUG="$DEBUG
+/usr/sup/bin/lua5.1 is executable"
+else
+  DEBUG="$DEBUG
+/usr/sup/bin/lua5.1 is NOT executable"
+fi
+
