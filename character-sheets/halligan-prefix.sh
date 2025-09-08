aa6025e201b91de5311c7350b7bbe3d2482247e6
Norman Ramsey <nr@cs.tufts.edu>
2025-09-07 20:35:04 -0400

    get paths that work on red hat 9

--- character-sheets/halligan-prefix.sh
+++ character-sheets/halligan-prefix.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export CHARSHEET_CMD=/h/nr/www/charsheet/charsheet
 export CHARSHEETS=/h/nr/www/charsheet/
 export TEXINPUTS=.:/h/nr/www/charsheet/:
-export PATH="/home/nr/bin:/usr/sup/bin:$PATH"
+export PATH="/usr/local/bin:/home/nr/bin:/usr/sup/bin:/usr/local/texlive/2025/bin/x86_64-linux:$PATH"
 export LUA_PATH="/h/nr/src/lua/?.lua;/h/nr/src/lua/?/init.lua;/h/nr/lib/lua/5.1/?.lua;/usr/unsup/nr/lib/lua5.1/?.lua;/usr/unsup/nr/lib/lua5.1/?/init.lua;/h/nr/.luarocks/share/lua/5.1/?/init.lua;/h/nr/.luarocks/share/lua/5.1/?.lua;;"
 export LUA_CPATH="/h/nr/machine/amd64-linux/lib/?.so;/h/nr/machine/amd64-linux/lib/lua5.1/?.so;/usr/unsup/nr/lib/lua5.1/?.so;/h/nr/.luarocks/lib/lua/5.1/?.so;;"
 
@@ -13,10 +13,9 @@ LUA_PATH=$LUA_PATH
 LUA_CPATH=$LUA_CPATH
 which lua.51: $(type lua5.1)"
 
-if [[ -x /usr/sup/bin/lua5.1 ]]; then
-  DEBUG="$DEBUG
-/usr/sup/bin/lua5.1 is executable"
-else
+lua="$(which lua5.1)"
+
+if [[ ! -x "$lua" ]]; then
   DEBUG="$DEBUG
 /usr/sup/bin/lua5.1 is NOT executable"
 fi
